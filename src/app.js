0a5ec2e42ab92aa4f8073545e8ab998b282b4657
Javier Onishi <javier-onishi@outlook.com>
2026-07-29 13:49:17 -0400

    se agrego morgan

--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
 import cors from 'cors';
 import express from 'express';
 import helmet from 'helmet';
+import morgan from 'morgan';
 import swaggerUi from 'swagger-ui-express';
 
-import { errorHandler } from './middlewares/error.middleware.js';
-import { requestLogger } from './middlewares/logger.middleware.js';
-import routes from './routes/index.routes.js';
 import swaggerSpec from './config/swagger.js';
+import { errorHandler } from './middlewares/error.middleware.js';
+import routes from './routes/index.routes.js';
 
 const app = express();
 
 app.use(cors());
 app.use(helmet());
+app.use(morgan('combined'));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(express.json());
-app.use(requestLogger);
 app.use(routes);
 
 // Manejador para rutas no encontradas (404)
